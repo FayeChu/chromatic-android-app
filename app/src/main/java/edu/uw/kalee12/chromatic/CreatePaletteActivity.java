e86cf1f08f12cb8acb591c6365be9607140e7072
Karen Lee <leekaren888@gmail.com>
2017-11-28 11:11:24 -0800

    Implemented Create Palette

--- app/src/main/java/edu/uw/kalee12/chromatic/CreatePaletteActivity.java
+++ app/src/main/java/edu/uw/kalee12/chromatic/CreatePaletteActivity.java
@@ -1,13 +1,26 @@
 package edu.uw.kalee12.chromatic;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CreatePaletteActivity extends AppCompatActivity {
+
+    private Button pressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +28,141 @@ public class CreatePaletteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_palette);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Button colorOne = (Button) findViewById(R.id.color_one);
+        colorOne.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    set(colorOne);
+                }
+                return true;
+            }
+        });
+
+        final Button colorTwo = (Button) findViewById(R.id.color_two);
+        colorTwo.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    set(colorTwo);
+                }
+                return true;
+            }
+        });
+
+        final Button colorThree = (Button) findViewById(R.id.color_three);
+        colorThree.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    set(colorThree);
+                }
+                return true;
+            }
+        });
+
+        final Button colorFour = (Button) findViewById(R.id.color_four);
+        colorFour.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    set(colorFour);
+                }
+                return true;
+            }
+        });
+
+        final Button colorFive = (Button) findViewById(R.id.color_five);
+        colorFive.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    set(colorFive);
+                }
+                return true;
+            }
+        });
+
+        final ImageView image = (ImageView) findViewById(R.id.photo_box);
+        final Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+        image.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN ||
+                        motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+                    float[] points = getPointerCoords(image, motionEvent);
+                    if (points[0] > 0 && points[0] < bitmap.getWidth()
+                            && points[1] > 0 && points[1] < bitmap.getHeight()) {
+                        int color = bitmap.getPixel((int) points[0], (int) points[1]);
+                        if (pressed != null) {
+                            pressed.setBackgroundColor(color);
+                        }
+                    }
+                }
+                return true;
             }
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_create_palette, menu);
+        return true;
+    }
+
+    final float[] getPointerCoords(ImageView view, MotionEvent e)
+    {
+        final int index = e.getActionIndex();
+        final float[] coords = new float[] { e.getX(index), e.getY(index) };
+        Matrix matrix = new Matrix();
+        view.getImageMatrix().invert(matrix);
+        matrix.postTranslate(view.getScrollX(), view.getScrollY());
+        matrix.mapPoints(coords);
+        return coords;
+    }
+
+    public void set(Button button) {
+        button.setPressed(!button.isPressed());
+        if (pressed != null) {
+            pressed.setPressed(false);
+        }
+        if (button.isPressed()) {
+            pressed = button;
+        } else {
+            pressed = null;
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.next:
+                Intent intent = new Intent(CreatePaletteActivity.this,
+                        SetPaletteInfoActivity.class);
+
+                Button[] buttons = new Button[] {findViewById(R.id.color_one), findViewById(R.id.color_two),
+                        findViewById(R.id.color_three), findViewById(R.id.color_four),
+                        findViewById(R.id.color_five)};
+
+                int[] colors = new int[5];
+                for (int i = 0; i < buttons.length; i++) {
+                    ColorDrawable buttonColor = (ColorDrawable) buttons[i].getBackground();
+                    colors[i] = buttonColor.getColor();
+                }
+                intent.putExtra("colors", colors);
+
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
 
 /*
--- app/src/main/java/edu/uw/kalee12/chromatic/SetPaletteInfoActivity.java
+++ app/src/main/java/edu/uw/kalee12/chromatic/SetPaletteInfoActivity.java
@@ -1,11 +1,16 @@
 package edu.uw.kalee12.chromatic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class SetPaletteInfoActivity extends AppCompatActivity {
 
@@ -15,15 +20,34 @@ public class SetPaletteInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_palette_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        int[] colors = getIntent().getIntArrayExtra("colors");
+        TextView[] boxes = new TextView[] {findViewById(R.id.set_one), findViewById(R.id.set_two),
+                findViewById(R.id.set_three), findViewById(R.id.set_four),
+                findViewById(R.id.set_five)};
+
+        for (int i = 0; i < colors.length; i++) {
+            boxes[i].setBackgroundColor(colors[i]);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_save, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
