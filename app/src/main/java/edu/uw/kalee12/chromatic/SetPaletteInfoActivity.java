614b0b09c9f335802d3476f7b2920852ca0f86d5
Karen Lee <leekaren888@gmail.com>
2017-11-28 17:05:56 -0800

    Implemented Place Picker

--- app/src/main/java/edu/uw/kalee12/chromatic/SetPaletteInfoActivity.java
+++ app/src/main/java/edu/uw/kalee12/chromatic/SetPaletteInfoActivity.java
@@ -1,16 +1,32 @@
 package edu.uw.kalee12.chromatic;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class SetPaletteInfoActivity extends AppCompatActivity {
 
@@ -31,6 +47,44 @@ public class SetPaletteInfoActivity extends AppCompatActivity {
         for (int i = 0; i < colors.length; i++) {
             boxes[i].setBackgroundColor(colors[i]);
         }
+
+        ImageButton location = (ImageButton) findViewById(R.id.btn_location);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(SetPaletteInfoActivity.this), 1);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        ImageButton date = (ImageButton) findViewById(R.id.btn_date);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getFragmentManager(), "datePicker");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this, data);
+                TextView location = (TextView) findViewById(R.id.txt_location);
+                location.setText(place.getName() + " " + place.getAddress());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
@@ -48,6 +102,27 @@ public class SetPaletteInfoActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+                                    implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
+            dialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+            return dialog;
+        }
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            TextView date = (TextView) getActivity().findViewById(R.id.txt_date);
+            date.setText(month + "/" + day + "/" + year);
+        }
     }
 
 }
