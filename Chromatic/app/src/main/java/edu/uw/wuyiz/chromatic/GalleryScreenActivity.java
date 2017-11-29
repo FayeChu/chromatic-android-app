4ffbd92e444ededccac94d39c21f41f73a0178f8
Faye <chuxiaofei0725@gmail.com>
2017-11-29 00:32:24 -0800

    Fix action bar

--- Chromatic/app/src/main/java/edu/uw/wuyiz/chromatic/GalleryScreenActivity.java
+++ Chromatic/app/src/main/java/edu/uw/wuyiz/chromatic/GalleryScreenActivity.java
@@ -23,6 +23,7 @@ import com.lwj.widget.picturebrowser.PictureFragment;
 import com.lwj.widget.picturebrowser.PictureLoader;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryScreenActivity extends AppCompatActivity {
 
@@ -39,7 +40,6 @@ public class GalleryScreenActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         uri = intent.getStringExtra("uri");
-        //imageUri = Uri.parse(uri);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
@@ -71,13 +71,6 @@ public class GalleryScreenActivity extends AppCompatActivity {
         if (uri != null) {
             mUrlList.add(uri);
         }
-
-//        mUrlList.add("http://img1.imgtn.bdimg.com/it/u=963551012,3660149984&fm=214&gp=0.jpg");
-//        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510081122984&di=280211f10d59e3edf8e1221b8ccad564&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3Df9871d34de0735fa85fd46faf63865c6%2Fe7cd7b899e510fb35dbbc8b8d333c895d1430c7a.jpg");
-//        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510081221368&di=48b177e870ba6161b4c055fe41fce56b&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3D24a713ecb8119313d34ef7f30d5166a2%2Fb17eca8065380cd7e6c77fc3ab44ad34588281c7.jpg");
-//        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510081254731&di=9211817b5c8aa01e93fedd38e06311d7&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2F2015%2Fa2%2F228%2Fd%2F134.jpg");
-//        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510081307059&di=b3168456428fcaf75f5209f8643aea8a&imgtype=0&src=http%3A%2F%2Fqiniu.usitrip.com%2Fimages%2Fckfinder%2Fimages%2Fchongwu_20150507.jpg");
-//        mUrlList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510081351061&di=97662ce6fb6f44267766610c94489e35&imgtype=0&src=http%3A%2F%2Fimg.taopic.com%2Fuploads%2Fallimg%2F110914%2F8879-110914234S561.jpg");
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
@@ -137,7 +130,6 @@ public class GalleryScreenActivity extends AppCompatActivity {
 
         Uri uri = savedInstanceState.getParcelable(KEY_IMAGE_URI);
         if (uri != null) {
-            //onImagePicked(uri);
             Intent intent = new Intent(this, MoodPreviewActivity.class);
             intent.putExtra("uri", uri.toString());
             startActivity(intent);
