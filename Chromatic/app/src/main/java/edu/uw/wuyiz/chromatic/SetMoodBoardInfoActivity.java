716e54169c0f6dbc5e75f9bfafbd6a71d57a2045
Wuyi Zhang <wuyi_zhang@outlook.com>
2017-11-28 22:47:00 -0800

    Fixed conflict

--- Chromatic/app/src/main/java/edu/uw/wuyiz/chromatic/SetMoodBoardInfoActivity.java
+++ Chromatic/app/src/main/java/edu/uw/wuyiz/chromatic/SetMoodBoardInfoActivity.java
@@ -12,7 +12,7 @@ public class SetMoodBoardInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_mbinfo);
+        setContentView(R.layout.activity_set_mood_board_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
